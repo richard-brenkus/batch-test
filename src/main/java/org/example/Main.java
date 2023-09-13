748eb2cb55d922e94c92d018bc7d8408f02b27c2
richard-brenkus <richard_brenkus@yahoo.com>
2023-09-13 18:01:07 +0200

    writing to DB works

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("is this thing working yet?");
+        SpringApplication.run(Main.class, args);
+    }
+}
