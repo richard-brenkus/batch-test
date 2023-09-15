210d4f75a957d9cb12aa7d2a3cd403d0c27aaef4
richard-brenkus <richard_brenkus@yahoo.com>
2023-09-15 18:43:33 +0200

    added dynamic file upload

--- src/main/java/org/example/config/SpringBatchConfig.java
+++ src/main/java/org/example/config/SpringBatchConfig.java
@@ -4,29 +4,30 @@ import lombok.AllArgsConstructor;
 import org.example.entity.Customer;
 import org.example.listener.StepSkipListener;
 import org.example.partition.ColumnRangePartitioner;
-import org.example.repository.CustomerRepository;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.SkipListener;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.partition.PartitionHandler;
 import org.springframework.batch.core.partition.support.TaskExecutorPartitionHandler;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.skip.SkipPolicy;
-import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import java.io.File;
 
 @Configuration
 //@EnableBatchProcessing
@@ -44,9 +45,11 @@ public class SpringBatchConfig {
     //partitioning:
     private CustomerWriter customerWriter;
 
-    public FlatFileItemReader<Customer> reader(){
+    @Bean
+    @StepScope
+    public FlatFileItemReader<Customer> itemReader(@Value("#{jobParameters[fullPathFileName]}") String pathToFile){
         FlatFileItemReader<Customer> itemReader = new FlatFileItemReader<>();
-        itemReader.setResource(new FileSystemResource("src/main/resources/customers.csv"));
+        itemReader.setResource(new FileSystemResource(new File(pathToFile)));
         itemReader.setName("csvReader");
         itemReader.setLinesToSkip(1);
         itemReader.setLineMapper(lineMapper());
@@ -90,19 +93,19 @@ public class SpringBatchConfig {
     }
 
     @Bean
-    public PartitionHandler partitionHandler(){
+    public PartitionHandler partitionHandler(FlatFileItemReader<Customer> itemReader){
         TaskExecutorPartitionHandler taskExecutorPartitionHandler = new TaskExecutorPartitionHandler();
         taskExecutorPartitionHandler.setGridSize(4);
         taskExecutorPartitionHandler.setTaskExecutor(taskExecutor());
-        taskExecutorPartitionHandler.setStep(workerStep());
+        taskExecutorPartitionHandler.setStep(workerStep(itemReader));
         return taskExecutorPartitionHandler;
     }
 
     @Bean
-    public Step workerStep() {
+    public Step workerStep(FlatFileItemReader<Customer> itemReader) {
         return new StepBuilder("worker-step", jobRepository)
                 .<Customer, Customer>chunk(250, transactionManager)
-                .reader(reader())
+                .reader(itemReader)
                 .processor(processor())
                 .writer(customerWriter)
                 .faultTolerant()
@@ -115,10 +118,10 @@ public class SpringBatchConfig {
     }
 
     @Bean
-    public Step managerStep() {
+    public Step managerStep(FlatFileItemReader<Customer> itemReader) {
         return new StepBuilder("manager-step", jobRepository)
-                .partitioner(workerStep().getName(), partitioner())
-                .partitionHandler(partitionHandler())
+                .partitioner(workerStep(itemReader).getName(), partitioner())
+                .partitionHandler(partitionHandler(itemReader))
                 .build();
     }
 
@@ -135,9 +138,9 @@ public class SpringBatchConfig {
     }*/
 
     @Bean
-    public Job runJob(){
+    public Job runJob(FlatFileItemReader<Customer> itemReader){
         return new JobBuilder("importCustomers", jobRepository)
-                .flow(managerStep())
+                .flow(managerStep(itemReader))
                 .end()
                 .build();
     }
--- src/main/java/org/example/controller/JobController.java
+++ src/main/java/org/example/controller/JobController.java
@@ -1,9 +1,6 @@
 package org.example.controller;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
@@ -11,7 +8,14 @@ import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/jobs")
@@ -23,15 +27,26 @@ public class JobController {
     @Autowired
     private Job job;
 
+    private final String TEMP_STORAGE = "C:\\Data\\Batch-files";
+
     @PostMapping("/importCustomers")
-    public void importCsvToDBJob() {
-        JobParameters jobParameters = new JobParametersBuilder()
-                .addLong("startAt", System.currentTimeMillis()).toJobParameters();
+    public void importCsvToDBJob(@RequestParam("file") MultipartFile multipartFile) {
 
         try {
-            jobLauncher.run(job, jobParameters);
+            String originalFileName = multipartFile.getOriginalFilename();
+            File fileToImport = new File(TEMP_STORAGE + originalFileName);
+            multipartFile.transferTo(fileToImport);
+
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addString("fullPathFileName", TEMP_STORAGE + originalFileName)
+                    .addLong("startAt", System.currentTimeMillis()).toJobParameters();
+            JobExecution jobExecution = jobLauncher.run(job, jobParameters);
+
+            if(jobExecution.getExitStatus().getExitCode().equals((ExitStatus.COMPLETED)))
+                Files.deleteIfExists(Paths.get(TEMP_STORAGE + originalFileName));
+
         } catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException |
-                 JobParametersInvalidException e) {
+                 JobParametersInvalidException | IOException e) {
             e.printStackTrace();
         }
     }
